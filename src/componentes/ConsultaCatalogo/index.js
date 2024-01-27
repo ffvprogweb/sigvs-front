081556e26bc9ecbd3f67d2b7d608d39454acbc92
ffvprogweb <disclabes@gmail.com>
2024-01-27 18:38:55 -0300

    Função - ConsultaCatalogo Changes to be committed: modified:   src/componentes/ConsultaCatalogo/index.js modified:   src/componentes/ConsultaCatalogo/styles.css

--- src/componentes/ConsultaCatalogo/index.js
+++ src/componentes/ConsultaCatalogo/index.js
@@ -2,31 +2,51 @@ import { useState, useEffect } from "react";
 import "./styles.css";
 const ConsultaCatalogo = () => {
   const [produtos, setProdutos] = useState([]);
+  const [erro, setErro] = useState(null);
   useEffect(() => {
     const consulta = async () => {
-      const resposta = await fetch("http://localhost:8080/api/v1/produtos");
-      const dados = await resposta.json(); //retorna um array de objetos json
-      //apresenta os dados na console como um string json
-      console.log(JSON.stringify(dados));
-      //carrega os dados na variavel produto
-      setProdutos(dados);
+      try {
+        const resposta = await fetch("http://localhost:8080/api/v1/produtos");
+        if (!resposta.ok) {
+          throw new Error();
+        }
+        const dados = await resposta.json(); //retorna um array de objetos json
+        //apresenta os dados na console como um string json
+        console.log(JSON.stringify(dados));
+        //carrega os dados na variavel produto
+        setProdutos(dados);
+      } catch (error) {
+        setErro(error.message);
+      }
     };
     consulta();
   }, []);
+  if (erro)
+    return (
+      <>
+        <h3>Consulta Catalogo </h3>
+        <p>Erro na consulta: {erro}</p>
+      </>
+    );
   return (
-    <div className="App">
-      <table>
+    <div>
+      <h3>Consulta Catalogo </h3>
+      <table className="Catalogo">
         <thead>
           <tr>
-            <th>id</th>
+            <th>ID</th>
             <th>Descrição</th>
+            <th>Custo</th>
+            <th>Quant</th>
           </tr>
         </thead>
         <tbody>
           {produtos.map((produto) => (
             <tr key={produto.id}>
               <td>{produto.id}</td>
-              <td className="Catalogo">{produto.descricao}</td>
+              <td>{produto.descricao}</td>
+              <td>{produto.custo}</td>
+              <td>{produto.quantidadeNoEstoque}</td>
             </tr>
           ))}
         </tbody>
